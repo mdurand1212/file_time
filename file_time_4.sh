89d335d844023650ab7b20926a75de89a42e0aae
mdurand1212 <46043996+mdurand1212@users.noreply.github.com>
2020-11-18 02:17:15 -0800

    Add files via upload

---
+++ file_time_4.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+#=============================================
+# Script Name: file_time_3
+# By:          MD
+# Date:        September 30, 2020
+# Purpose:     Monitor file/folder interaction
+#              consits of:
+#               path and sendmail
+#=============================================
+echo "Enter a filepath or folder: "
+# enter a folder or file
+read file_folder 
+# take data input 
+# stat file_folder
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "Stat: "
+stat $file_folder
+echo "++++++++++++++++++++++++++++++++++++++++++"
+echo "Inotify: "
+inotifywait $file_folder
+# if a watch is triggerd send an email
+# sendmail mdurand1212@gmail.com
+echo "+++++++++++++++++++++++++++++++++++++++++"
+echo "The file or folder is--> " $file_folder
+echo "Thanks for checking timestamps"
